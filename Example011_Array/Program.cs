4a63a0ae07d64afefbaf2c20c36e876c69f40d3d
Tatiana <ttt-ttt@bk.ru>
2022-07-02 01:29:26 +0300

    ex11

---
+++ Example011_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿// Имеется одномерный массив array из n элементов. Требуется найти номер элемента массива, равного find.
+int[] array = { 12, 15, 18, 34, 12, 67 };
+int index = 0;
+int find = 12;
+int n = array.Length;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
