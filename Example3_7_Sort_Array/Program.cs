17406a7fb942fe69825c3d24c2488f9757b109d5
Tatiana <ttt-ttt@bk.ru>
2022-07-10 01:28:03 +0300

    Sort array min or max

---
+++ Example3_7_Sort_Array/Program.cs
@@ -0,0 +1,32 @@
+﻿// Сортировка массива методом выбора
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int k = i + 1; k < array.Length; k++)
+        {
+            if (array[k] < array[minPosition])
+                minPosition = k;
+        }
+        int bufer = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = bufer;
+    }
+}
+
+
+
+int[] array = {3, 6, 9, 8, 4, 5, 4, 3, 1, 2};
+PrintArray(array);
+SelectionSort(array);
+PrintArray(array);
---
+++ Examples3_8_SortMaxArray/Program.cs
@@ -0,0 +1,32 @@
+﻿// Сортировка массива методом выбора от максимального к минимальному
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int k = i + 1; k < array.Length; k++)
+        {
+            if (array[k] > array[maxPosition])
+                maxPosition = k;
+        }
+        int bufer = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = bufer;
+    }
+}
+
+
+
+int[] array = {3, 6, 9, 8, 4, 5, 4, 3, 1, 2};
+PrintArray(array);
+SelectionSort(array);
+PrintArray(array);
