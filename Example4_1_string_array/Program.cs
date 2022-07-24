76f04873f306fd66ce7b50e66fa6209b82b4d023
Tatiana <ttt-ttt@bk.ru>
2022-07-24 23:40:39 +0300

    two-dimensional arrays

---
+++ Example4_1_string_array/Program.cs
@@ -0,0 +1,14 @@
+﻿// Создание двумерного массива строк
+string[,] array = new string [2, 5];
+// По умолчанию массив заполняется пустыми строками константой String.Empty
+
+array[1, 2] = "слово";
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write($"-{array[i, j]}- ");
+    }
+    Console.WriteLine(" ");
+
+}
---
+++ Example4_2_int_array/Program.cs
@@ -0,0 +1,32 @@
+﻿// Создание двумерного массива чисел
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+void FillArray(int[,] array)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int [3, 4];
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
